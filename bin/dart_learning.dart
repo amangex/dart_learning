c94ced2d6b15cfa871fb640b0b715f58c4c360fb
amangex <amangexon@gmail.com>
2023-06-27 00:38:07 +0600

    comparing dates

--- bin/dart_learning.dart
+++ bin/dart_learning.dart
@@ -1,6 +1,9 @@
 void main() {
  var DateTime1 = DateTime.parse('2023-06-27 00:25:37');
  var DateTime2 = DateTime.parse('2023-06-27 00:25:35');
+ var DateTime3 = DateTime2.add(Duration(seconds: 2));
 
- print(DateTime1.difference(DateTime2));
+ print(DateTime1.compareTo(DateTime3));
+ print(DateTime2);
+ print(DateTime3);
 }
