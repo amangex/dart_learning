a0705488137a49f489ae7ba40e2667180890838b
amangex <amangexon@gmail.com>
2023-06-28 00:30:55 +0600

    collection list pt.2

--- bin/dart_learning.dart
+++ bin/dart_learning.dart
@@ -1,12 +1,11 @@
 void main() {
- List numbers1 = [1,2,3];
- List numbers2 = [];
+ final List numbers1 = <int>[1, 2, 3];
+ List numbers2 = [for(var i in numbers1) i * i, 9, 4, 1];
 
- numbers2.add(2);
+ numbers1.add(4);
 
-
- print(numbers1.length);
- print(numbers2.length);
+ // print(numbers1.length);
+ // print(numbers2.length);
  print(numbers1);
  print(numbers2);
 }
