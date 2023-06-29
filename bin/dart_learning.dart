28bd31347458af8b3241a90990f18e2319e2225f
amangex <amangexon@gmail.com>
2023-06-30 00:12:33 +0600

    OOP in dart using classes

--- bin/dart_learning.dart
+++ bin/dart_learning.dart
@@ -1,8 +1,18 @@
 void main() {
-  var article = Article();
-  article.title = 'OOP IN DART';
-  article.body = 'i hope that i understood what is oop in dart';
+ // var article = Article();
+ // article.title = 'OOP IN DART';
+ // article.body = 'i hope that i understood what is oop in dart';
 
+
+
+ // print(article.getArticleInfo());
+
+   var article = Article()
+  ..title = 'OOP IN DART'
+  ..body =
+      'i hope that i understood what is oop in dart'
+  ..init1()
+  ..init2();
 
 
   print(article.getArticleInfo());
@@ -12,6 +22,9 @@ void main() {
 class Article {
   String? title;
   String? body;
+
+  void init1() => print('init1');
+  void init2() => print('init2');
 
   String getArticleInfo() => '${title}\n${body}';
 }
