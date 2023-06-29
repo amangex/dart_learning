70a30c171953e45089568361f02927147a66bb55
amangex <amangexon@gmail.com>
2023-06-29 22:28:36 +0600

    practicing functions

--- bin/dart_learning.dart
+++ bin/dart_learning.dart
@@ -1,11 +1,35 @@
-
 void main() {
-  var count = 0;
-  do {
-    count++;
+  start();
+  var func = start;
+  func();
+  var Username = getUserName('Alex', 'amx');
+  PrintUserInfo(Username, 'Admin', 'as@gmail.com');
+  PrintUserInfoNamedParams( Username, role: 'Admin');
 
-    if (count % 2 == 0) continue;
+  int sum(int a, int b) => a + b;
 
-    print(count);
-  }while(count < 5);
+  doWork(sum);
+}
+
+void start() => print('start');
+
+void PrintUserInfo(String? username, [String? role = 'User', String? email = 'unknown']) {
+  print('Username - $username, Role - $role, Email - $email');
+} 
+
+
+void PrintUserInfoNamedParams(String? username,
+  {String? role = 'User', String? email = 'unknown'}) {
+  print('Username - $username, Role - $role, Email - $email');
+} 
+
+String getUserName(String Firstname, String Lastname){
+  return '$Firstname$Lastname'.toLowerCase();
+}
+
+
+void doWork(int Function(int, int) callback) {
+  var result = callback(1, 2);
+
+  print(result);
 }
