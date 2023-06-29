a71bc559fa48abdeb84248a77a5d30f94cfbdbb4
amangex <amangexon@gmail.com>
2023-06-29 22:41:02 +0600

    bool removecallback

--- bin/dart_learning.dart
+++ bin/dart_learning.dart
@@ -1,35 +1,13 @@
 void main() {
-  start();
-  var func = start;
-  func();
-  var Username = getUserName('Alex', 'amx');
-  PrintUserInfo(Username, 'Admin', 'as@gmail.com');
-  PrintUserInfoNamedParams( Username, role: 'Admin');
+  var numbers = <int>[1, 2, 3];
+  var removeCallback = (int a) => numbers.remove(a);
 
-  int sum(int a, int b) => a + b;
-
-  doWork(sum);
+  doWork(removeCallback);
+  print(numbers);
 }
 
-void start() => print('start');
-
-void PrintUserInfo(String? username, [String? role = 'User', String? email = 'unknown']) {
-  print('Username - $username, Role - $role, Email - $email');
-} 
-
-
-void PrintUserInfoNamedParams(String? username,
-  {String? role = 'User', String? email = 'unknown'}) {
-  print('Username - $username, Role - $role, Email - $email');
-} 
-
-String getUserName(String Firstname, String Lastname){
-  return '$Firstname$Lastname'.toLowerCase();
-}
-
-
-void doWork(int Function(int, int) callback) {
-  var result = callback(1, 2);
+void doWork(bool Function(int) callback) {
+  var result = callback(1);
 
   print(result);
 }
